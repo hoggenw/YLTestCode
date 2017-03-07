f17930e82d2b7ee620e2d70c5860bef473dcd8fd
dev-wangliugen <253192463@qq.com>
2017-03-07 13:46:40 +0800

    aesEncrypt

---
+++ YLTest/YLTestCode/String+Extension.swift
@@ -0,0 +1,87 @@
+//
+//  String+Extension.swift
+//  AESTest
+//
+//  Created by 王留根 on 17/3/6.
+//  Copyright © 2017年 ios-mac. All rights reserved.
+//
+
+import Foundation
+let IVString = "92170GzS8e64I28z"
+extension String {
+    
+    func aesEncrypt(key:String,_ iv:String = IVString, options:Int = kCCOptionPKCS7Padding) -> String? {
+        if let keyData = key.data(using: String.Encoding.utf8),
+            let data = self.data(using: String.Encoding.utf8),
+            let cryptData    = NSMutableData(length: Int((data.count)) + kCCBlockSizeAES128) {
+            
+            let keyLength              = size_t(kCCKeySizeAES128)
+            let operation: CCOperation = UInt32(kCCEncrypt)
+            let algoritm:  CCAlgorithm = UInt32(kCCAlgorithmAES128)
+            let options:   CCOptions   = UInt32(options)
+            
+            var numBytesEncrypted :size_t = 0
+            
+            let cryptStatus = CCCrypt(operation,
+                                      algoritm,
+                                      options,
+                                      keyData.bytes, keyLength,
+                                      iv,
+                                      data.bytes, data.count,
+                                      cryptData.mutableBytes, cryptData.length,
+                                      &numBytesEncrypted)
+            
+            if UInt32(cryptStatus) == UInt32(kCCSuccess) {
+                cryptData.length = Int(numBytesEncrypted)
+                let base64cryptString = cryptData.base64EncodedString(options: .endLineWithLineFeed)
+                return base64cryptString
+            }
+            else {
+                return nil
+            }
+        }
+        return nil
+    }
+    
+    
+    func aesDecrypt(key:String, iv:String = IVString, options:Int = kCCOptionPKCS7Padding) -> String? {
+        if let keyData = key.data(using: String.Encoding.utf8),
+            let data = NSData(base64Encoded: self, options: .ignoreUnknownCharacters),
+            let cryptData    = NSMutableData(length: Int((data.length)) + kCCBlockSizeAES128) {
+            
+            let keyLength              = size_t(kCCKeySizeAES128)
+            let operation: CCOperation = UInt32(kCCDecrypt)
+            let algoritm:  CCAlgorithm = UInt32(kCCAlgorithmAES128)
+            let options:   CCOptions   = UInt32(options)
+            
+            var numBytesEncrypted :size_t = 0
+            
+            let cryptStatus = CCCrypt(operation,
+                                      algoritm,
+                                      options,
+                                      keyData.bytes, keyLength,
+                                      iv,
+                                      data.bytes, data.length,
+                                      cryptData.mutableBytes, cryptData.length,
+                                      &numBytesEncrypted)
+            
+            if UInt32(cryptStatus) == UInt32(kCCSuccess) {
+                cryptData.length = Int(numBytesEncrypted)
+                let unencryptedMessage = String(data: cryptData as Data, encoding:String.Encoding.utf8)
+                return unencryptedMessage
+            }
+            else {
+                return nil
+            }
+        }
+        return nil
+    }
+    
+}
+
+
+extension Data {
+    public var bytes:UnsafeRawPointer{
+        return (self as NSData).bytes
+    }
+}
