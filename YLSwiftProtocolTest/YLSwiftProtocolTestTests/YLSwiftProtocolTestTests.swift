af4a987f30e167f7b6fd332410b7fc65a1c9c534
dev-wangliugen <253192463@qq.com>
2017-03-13 08:36:13 +0800

    AES

--- YLSwiftProtocolTest/YLSwiftProtocolTestTests/YLSwiftProtocolTestTests.swift
+++ YLSwiftProtocolTest/YLSwiftProtocolTestTests/YLSwiftProtocolTestTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class YLSwiftProtocolTestTests: XCTestCase {
     
+    var testString = "123456224321"
+    var key  = "1234567890123456"
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,6 +24,8 @@ class YLSwiftProtocolTestTests: XCTestCase {
     }
     
     func testExample() {
+        let result = testString.aesEncrypt(key: key)?.aesDecrypt(key: key)
+        XCTAssertEqual(testString, result)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
