d2f1de2471d20cbb7aeffa3a463500dc9db29afd
dev-wangliugen <253192463@qq.com>
2017-05-01 09:26:20 +0800

    WkWebview test

---
+++ WKWebViewTest/WKWebViewTest/ViewController.swift
@@ -0,0 +1,201 @@
+//
+//  ViewController.swift
+//  WKWebViewTest
+//
+//  Created by 王留根 on 2017/4/29.
+//  Copyright © 2017年 ios-mac. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+let progressKey = "progressKey"
+
+class ViewController: UIViewController {
+
+    var webView: WKWebView!
+    let strUrl = "http://api.qjtv2.pisendev.com:8011/xin/index?token="
+    //"https://www.baidu.com/"
+    //
+//   // var webView: UIWebView!
+//    var progressView: UIProgressView!
+    
+    deinit {
+         webView.removeObserver(self, forKeyPath: progressKey)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //创建webview
+        //创建一个webview的配置项
+        let configuretion = WKWebViewConfiguration();
+        // Webview的偏好设置
+        configuretion.preferences = WKPreferences()
+        configuretion.preferences.minimumFontSize = 10
+        configuretion.preferences.javaScriptEnabled = true
+        // 默认是不能通过JS自动打开窗口的，必须通过用户交互才能打开
+        configuretion.preferences.javaScriptCanOpenWindowsAutomatically = false
+        configuretion.preferences.javaScriptEnabled = true
+        //通过js与webview内容交互配置
+        configuretion.userContentController = WKUserContentController();
+        //添加一个名称就可以在js通过这个名称发消息：
+        
+        webView = WKWebView(frame: CGRect(x:0, y:0, width:UIScreen.main.bounds.width, height:UIScreen.main.bounds.height),configuration:configuretion)
+        webView.addObserver(self, forKeyPath: progressKey, options: .new, context: nil)
+        webView.backgroundColor = UIColor.clear
+        webView.scrollView.bounces = true
+        webView.scrollView.alwaysBounceVertical = true
+        webView.allowsBackForwardNavigationGestures = true
+        webView.isMultipleTouchEnabled = true
+       // if #available(iOS 9.0, *) {webView.allowsLinkPreview = true}
+        webView.navigationDelegate = self;
+        webView.uiDelegate = self;
+        view.addSubview(webView)
+       
+//        webView.configuration.userContentController.addUserScript(WKUserScript(source: "window.webkit.messageHandlers.openItemDetail.postMessage({body: ''});", injectionTime: .atDocumentEnd, forMainFrameOnly: false))
+        configuretion.userContentController.add(self, name: "openItemDetail");
+        
+        
+//        webView.evaluateJavaScript(" ") { (repond, error) in
+//            
+//        }
+       
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        webView.load(URLRequest(url: URL(string: strUrl)!))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        print(",newProgress = ")
+    }
+//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+//        print(",newProgress = ")
+//        if keyPath==progressKey {
+//            let newProgress  = Float(self.webView.estimatedProgress)
+//            print(",newProgress = \(newProgress)")
+//
+//        }
+//    }
+
+
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let urlString = navigationAction.request.url?.absoluteString
+        print("\(String(describing: urlString))")
+        decisionHandler(.allow)
+        
+        
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+    }
+//    
+//    // 决定导航的动作，通常用于处理跨域的链接能否导航。WebKit对跨域进行了安全检查限制，不允许跨域，因此我们要对不能跨域的链接
+//    // 单独处理。但是，对于Safari是允许跨域的，不用这么处理。
+//    // 这个是决定是否Request
+//    - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler;
+//    
+//    // 决定是否接收响应
+//    // 这个是决定是否接收response
+//    // 要获取response，通过WKNavigationResponse对象获取
+//    - (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler;
+//    
+//    // 当main frame的导航开始请求时，会调用此方法
+//    - (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(null_unspecified WKNavigation *)navigation;
+//    
+//    // 当main frame接收到服务重定向时，会回调此方法
+//    - (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(null_unspecified WKNavigation *)navigation;
+//    
+//    // 当main frame开始加载数据失败时，会回调
+//    - (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error;
+//    
+//    // 当main frame的web内容开始到达时，会回调
+//    - (void)webView:(WKWebView *)webView didCommitNavigation:(null_unspecified WKNavigation *)navigation;
+//    
+//    // 当main frame导航完成时，会回调
+//    - (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation;
+//    
+//    // 当main frame最后下载数据失败时，会回调
+//    - (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error;
+//    
+//    // 这与用于授权验证的API，与AFN、UIWebView的授权验证API是一样的
+//    - (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *__nullable credential))completionHandler;
+//    
+//    // 当web content处理完成时，会回调
+//    - (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView NS_AVAILABLE(10_11, 9_0);
+
+
+}
+
+extension ViewController: WKUIDelegate {
+//    // 创建新的webview
+//    // 可以指定配置对象、导航动作对象、window特性
+//    - (nullable WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures;
+//    
+//    // webview关闭时回调
+//    - (void)webViewDidClose:(WKWebView *)webView NS_AVAILABLE(10_11, 9_0);
+//    
+//    // 调用JS的alert()方法
+//    - (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler;
+//    
+//    // 调用JS的confirm()方法
+//    - (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL result))completionHandler;
+//    
+//    // 调用JS的prompt()方法
+//    - (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * __nullable result))completionHandler;
+
+    
+}
+
+extension ViewController: WKScriptMessageHandler{
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "openItemDetail" {
+            print("message")
+        }
+    }
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ WKWebViewTest/WKWebViewTest/YLProgressView.swift
@@ -0,0 +1,159 @@
+//
+//  YLProgressView.swift
+//  YLVideoRecord
+//
+//  Created by 王留根 on 17/2/3.
+//  Copyright © 2017年 ios-mac. All rights reserved.
+//
+
+import UIKit
+
+
+open class YLProgressView: UIView {
+
+    
+    var shapeLayer :CAShapeLayer!
+    //进度条计时器时间间隔
+    var incInterval: TimeInterval = 0.05
+    //总的时间
+    var totalTimerInterval: TimeInterval?
+    //总时间标准
+    var totalTime: TimeInterval?
+    
+    var showProgress: CGFloat?
+    //进度开始标刻
+    var startProgress: CGFloat?
+    var timeLeftLabel = UILabel()
+    
+    public var progressTimer: Timer?
+    
+    deinit {
+        if progressTimer != nil {
+            progressTimer?.invalidate()
+            progressTimer = nil
+        }
+        print("progress deinit")
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.shapeLayer = CAShapeLayer(layer: self.layer)
+        shapeLayer.borderWidth = 1
+        shapeLayer.lineWidth = 4
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        timeLeftLabel.frame = CGRect(x: 0, y: shapeLayer.lineWidth, width: frame.width, height: frame.height - shapeLayer.lineWidth)
+        timeLeftLabel.textAlignment = .center
+        timeLeftLabel.textColor = UIColor.white
+        timeLeftLabel.adjustsFontSizeToFitWidth = true
+        tintColorDidChange()
+        self.layer.addSublayer(self.shapeLayer)
+        self.addSubview(timeLeftLabel)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //MARK: - Color
+    override open func tintColorDidChange() {
+        if (self.superclass?.instancesRespond(to: #selector(tintColorDidChange)))! {
+            super.tintColorDidChange()
+        }
+        
+        shapeLayer.strokeColor = UIColor.green.cgColor
+        shapeLayer.borderColor = UIColor.green.cgColor
+    }
+    
+    ///
+    ///
+    /// - Parameter progress: 输入进度条开始比例，不大于1
+    open func startProgress(progress: CGFloat ,totalTimer: TimeInterval) {
+        showProgress = progress
+        startProgress = progress
+        totalTimerInterval = totalTimer
+        totalTime = totalTimer
+        timeLeftLabel.text = String(format: "%.0lf″00", totalTimer)
+        progressTimer = Timer(timeInterval: incInterval, target: self, selector: #selector(timerProgress), userInfo: nil, repeats:true)
+        RunLoop.current.add(progressTimer!, forMode: RunLoopMode.defaultRunLoopMode)
+        
+        
+    }
+    
+    func timerProgress() {
+
+        totalTimerInterval = totalTimerInterval! - incInterval
+        showProgress = showProgress! - startProgress! * CGFloat(incInterval / totalTime!)
+        let second =  Int(totalTimerInterval! * 100 / 100)
+        let microsecond = Int(totalTimerInterval! * 100) % 100
+        timeLeftLabel.text = String(format: "%d″%2d", second,microsecond)
+        update(progress: showProgress!)
+        if totalTimerInterval! < 0 {
+            if progressTimer != nil {
+                progressTimer?.invalidate()
+                progressTimer = nil
+                print("over")
+                timeLeftLabel.text = ""
+                postNotification()
+            }
+        }
+        
+    }
+    
+    open func stopProgress() {
+        if progressTimer != nil {
+            progressTimer?.invalidate()
+            progressTimer = nil
+            print("over")
+            timeLeftLabel.text = ""
+            update(progress: 0)
+        }
+    }
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        shapeLayer.cornerRadius = frame.size.width / 2.0
+
+        shapeLayer.path = shapeLayerPath().cgPath
+    }
+    
+    open func update(progress: CGFloat) {
+        CATransaction.begin()
+        //显式事务默认开启动画效果,kCFBooleanTrue关闭
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        shapeLayer.strokeEnd = 1 - (1 - progress)/2
+        shapeLayer.strokeStart = 1 - shapeLayer.strokeEnd
+        CATransaction.commit()
+        
+    }
+    func postNotification() {
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "YLProgressTimeOver"), object: nil)
+    }
+    
+    func shapeLayerPath() -> UIBezierPath {
+        
+        let width = self.frame.size.width;
+        let borderWidth = self.shapeLayer.borderWidth;
+        
+        let path = UIBezierPath()
+        
+        path.lineWidth     = borderWidth
+        path.lineJoinStyle = .round //终点处理
+        path.move(to: CGPoint(x: 0, y: -2))
+        path.addLine(to: CGPoint(x: width, y: -2))
+        return path;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
