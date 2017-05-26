ca546208d9f4418322a11a9f91327d0308dadc4b
dev-wangliugen <253192463@qq.com>
2017-05-26 08:34:32 +0800

    Animation Test not finish

---
+++ YLAnimationTest/YLAnimationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YLAnimationTest
+//
+//  Created by 王留根 on 2017/5/23.
+//  Copyright © 2017年 ios-mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
