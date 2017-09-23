f33e5a35846eb3deba193584a81c178b8e86e5fd
hoggenw <253192463@qq.com>
2017-09-23 20:30:03 +0800

    chat test

---
+++ YLChatTest/YLChatTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLChatTest
+//
+//  Created by 王留根 on 2017/9/22.
+//  Copyright © 2017年 王留根. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLChatTest/YLChatTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLChatTest
+//
+//  Created by 王留根 on 2017/9/22.
+//  Copyright © 2017年 王留根. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
