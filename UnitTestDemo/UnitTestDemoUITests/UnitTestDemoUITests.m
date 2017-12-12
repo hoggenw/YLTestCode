a5d34774f6d17b9e721e973bf3b964296b3b573c
hoggenw <253192463@qq.com>
2017-12-12 10:11:09 +0800

    no message

--- UnitTestDemo/UnitTestDemoUITests/UnitTestDemoUITests.m
+++ UnitTestDemo/UnitTestDemoUITests/UnitTestDemoUITests.m
@@ -32,10 +32,10 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    
-    // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
+//- (void)testExample {
+//    
+//    // Use recording to get started writing UI tests.
+//    // Use XCTAssert and related functions to verify your tests produce the correct results.
+//}
 
 @end
--- UnitTextUIDemo/UnitTextUIDemoTests/UnitTextUIDemoTests.swift
+++ UnitTextUIDemo/UnitTextUIDemoTests/UnitTextUIDemoTests.swift
@@ -22,15 +22,24 @@ class UnitTextUIDemoTests: XCTestCase {
     }
     
     func testExample() {
+        testScoreIsComputed();
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testScoreIsComputed() {
+        
+        print("自定义测试testExample");
+        let  a = 3;
+        XCTAssertTrue(a == 0,"a 不能等于 0");
+        XCTAssertEqual(a, 95, "Score computed from guess is wrong")
     }
+    
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
