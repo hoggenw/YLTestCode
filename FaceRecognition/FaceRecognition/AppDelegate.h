7d64b53e84c3af8c58077984c118de969f3d2d2e
hoggenw <253192463@qq.com>
2018-08-02 17:02:51 +0800

    no message

---
+++ FaceRecognition/FaceRecognition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceRecognition
+//
+//  Created by 王留根 on 2018/8/2.
+//  Copyright © 2018年 hoggenWang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceRecognition/FaceRecognition/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FaceRecognition
+//
+//  Created by 王留根 on 2018/8/2.
+//  Copyright © 2018年 hoggenWang.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
