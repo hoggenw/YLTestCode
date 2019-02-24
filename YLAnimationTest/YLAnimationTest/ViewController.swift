b838daa3dbe0e2b4459876b420c751cece57f2b6
wanglg <253192463@qq.com>
2019-02-24 12:33:31 +0800

    no message

--- YLAnimationTest/YLAnimationTest/ViewController.swift
+++ YLAnimationTest/YLAnimationTest/ViewController.swift
@@ -25,57 +25,57 @@ class ViewController: UIViewController {
     
     func initialUI() {
         self.view.backgroundColor = UIColor.white;
-//        let button = UIButton()
-//        button.backgroundColor = UIColor.brown
-//        button.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 - 200, width: 60, height: 50)
-//        button.setTitle("circleAnimation", for: .normal)
-//
-//        button.addTarget(self, action: #selector(circleAnimationAction), for: .touchUpInside)
-//        self.view.addSubview(button)
-//
-//        let button1 = UIButton()
-//        button1.backgroundColor = UIColor.brown
-//        button1.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 - 100, width: 60, height: 50)
-//        button1.setTitle("波浪", for: .normal)
-//        button1.addTarget(self, action: #selector(waveAnimationAction), for: .touchUpInside)
-//        self.view.addSubview(button1)
-//
-//
-//        let button2 = UIButton()
-//        button2.backgroundColor = UIColor.brown
-//        button2.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 , width: 60, height: 50)
-//        button2.setTitle("3D", for: .normal)
-//        button2.addTarget(self, action: #selector(Animation3DAction), for: .touchUpInside)
-//        self.view.addSubview(button2)
-//
-//        let button3 = UIButton()
-//        button3.backgroundColor = UIColor.brown
-//        button3.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 + 100 , width: 60, height: 50)
-//        button3.setTitle("3D_Optimization", for: .normal)
-//        button3.addTarget(self, action: #selector(Animation3DViewAction), for: .touchUpInside)
-//        self.view.addSubview(button3)
-//
-//        //voiceInputViewAction
-//
-//        let button4 = UIButton()
-//        button4.backgroundColor = UIColor.brown
-//        button4.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 + 200 , width: 60, height: 50)
-//        button4.setTitle("voiceInput", for: .normal)
-//        button4.addTarget(self, action: #selector(voiceInputViewAction), for: .touchUpInside)
-//        self.view.addSubview(button4)
-//
+        let button = UIButton()
+        button.backgroundColor = UIColor.brown
+        button.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 - 200, width: 60, height: 50)
+        button.setTitle("circleAnimation", for: .normal)
+
+        button.addTarget(self, action: #selector(circleAnimationAction), for: .touchUpInside)
+        self.view.addSubview(button)
+
+        let button1 = UIButton()
+        button1.backgroundColor = UIColor.brown
+        button1.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 - 100, width: 60, height: 50)
+        button1.setTitle("波浪", for: .normal)
+        button1.addTarget(self, action: #selector(waveAnimationAction), for: .touchUpInside)
+        self.view.addSubview(button1)
+
+
+        let button2 = UIButton()
+        button2.backgroundColor = UIColor.brown
+        button2.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 , width: 60, height: 50)
+        button2.setTitle("3D", for: .normal)
+        button2.addTarget(self, action: #selector(Animation3DAction), for: .touchUpInside)
+        self.view.addSubview(button2)
+
+        let button3 = UIButton()
+        button3.backgroundColor = UIColor.brown
+        button3.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 + 100 , width: 60, height: 50)
+        button3.setTitle("3D_Optimization", for: .normal)
+        button3.addTarget(self, action: #selector(Animation3DViewAction), for: .touchUpInside)
+        self.view.addSubview(button3)
+
+        //voiceInputViewAction
+
+        let button4 = UIButton()
+        button4.backgroundColor = UIColor.brown
+        button4.frame = CGRect(x: self.view.bounds.width/2 - 30, y: self.view.bounds.height/2 + 200 , width: 60, height: 50)
+        button4.setTitle("voiceInput", for: .normal)
+        button4.addTarget(self, action: #selector(voiceInputViewAction), for: .touchUpInside)
+        self.view.addSubview(button4)
+
 //
 //        for index in 0 ..< addOne.count {
 //            print("changed number " +  String.init(format: "%d", addOne[index]) );
 //        }
 //
-        let button2 = UIButton()
-        button2.backgroundColor = UIColor.brown
-        button2.frame = CGRect(x: view.bounds.size.width/2 - 30, y: view.bounds.size.height/2 , width: 60, height: 50)
-        button2.setTitle("录制", for: .normal)
-        button2.titleLabel?.textColor = UIColor.white
-        self.view.addSubview(button2)
-        button2.addTarget(self, action: #selector(QR), for: .touchUpInside)
+//        let button2 = UIButton()
+//        button2.backgroundColor = UIColor.brown
+//        button2.frame = CGRect(x: view.bounds.size.width/2 - 30, y: view.bounds.size.height/2 , width: 60, height: 50)
+//        button2.setTitle("录制", for: .normal)
+//        button2.titleLabel?.textColor = UIColor.white
+//        self.view.addSubview(button2)
+//        button2.addTarget(self, action: #selector(QR), for: .touchUpInside)
 //
 //        let button3 = UIButton()
 //        button3.backgroundColor = UIColor.brown
