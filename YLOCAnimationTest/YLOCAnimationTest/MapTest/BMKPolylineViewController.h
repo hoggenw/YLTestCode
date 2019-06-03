ed4926f6612d8d8d6c1dc0dc8cfa7509ba92739f
wanglg <253192463@qq.com>
2019-06-03 11:19:29 +0800

    no message

---
+++ YLOCAnimationTest/YLOCAnimationTest/MapTest/BMKPolylineViewController.h
@@ -0,0 +1,13 @@
+//
+//  BMKPolylineViewController.h
+//  YLOCAnimationTest
+//
+//  Created by 王留根 on 2019/5/28.
+//  Copyright © 2019 hoggen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMKPolylineViewController : UIViewController
+
+@end
---
+++ YLOCAnimationTest/YLOCAnimationTest/MapTest/BMKPolylineViewController.m
@@ -0,0 +1,43 @@
+//
+//  BMKPolylineViewController.m
+//  YLOCAnimationTest
+//
+//  Created by 王留根 on 2019/5/28.
+//  Copyright © 2019 hoggen. All rights reserved.
+//
+
+#import "BMKPolylineViewController.h"
+
+@interface BMKPolylineViewController ()
+
+@end
+
+@implementation BMKPolylineViewController
+
+
+#pragma mark - Override Methods
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+#pragma mark - Public Methods
+
+
+#pragma mark - Events
+
+
+#pragma mark - Private Methods
+
+
+#pragma mark - Extension Delegate or Protocol
+
+@end
