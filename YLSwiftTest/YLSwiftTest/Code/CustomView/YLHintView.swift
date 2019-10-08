2d82e0b9ed8852301e737277340bdd48320a068d
hoggenw <253192463@qq.com>
2019-10-08 16:56:25 +0800

    no message

---
+++ YLSwiftTest/YLSwiftTest/Code/CustomView/YLHintView.swift
@@ -0,0 +1,21 @@
+//
+//  YLHintView.swift
+//  YLSwiftTest
+//
+//  Created by 王留根 on 2019/9/30.
+//  Copyright © 2019 hoggen.com. All rights reserved.
+//
+
+import UIKit
+
+class YLHintView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ YLSwiftTest/YLSwiftTest/Code/CustomView/YLLabel.swift
@@ -0,0 +1,21 @@
+//
+//  YLLabel.swift
+//  YLSwiftTest
+//
+//  Created by 王留根 on 2019/9/30.
+//  Copyright © 2019 hoggen.com. All rights reserved.
+//
+
+import UIKit
+
+class YLLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
